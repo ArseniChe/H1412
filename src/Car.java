dbbaa5f267055924e0bff1d6ec95b50b2e3cc889
Arseni <cherkasov.arseni@gmail.com>
2023-12-14 14:49:16 +0100

    Car

---
+++ src/Car.java
@@ -0,0 +1,7 @@
+public class Car extends Vehicle{
+    private String model;
+    public Car(String brand, int year, String model){
+        super(brand, year);
+        this.model = model;
+    }
+}
